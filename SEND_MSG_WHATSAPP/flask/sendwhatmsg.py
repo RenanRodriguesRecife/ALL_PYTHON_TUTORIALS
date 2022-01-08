197d19244cac268fdbc1d71e83cc84cd3dc869f1
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-01-08 11:54:06 -0300

    create script reload

---
+++ SEND_MSG_WHATSAPP/flask/sendwhatmsg.py
@@ -0,0 +1,6 @@
+import pywhatkit as kit
+
+kit.sendwhatmsg("+55***********","oi",11,34)
+
+# kit.sendwhatmsg("número que irá receber a msg","msg",hora,minutos)
+# é preciso está com o whatsapp web aberto
