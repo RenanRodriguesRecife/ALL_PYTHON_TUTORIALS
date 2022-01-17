da1fae7c102319578e181ff0af24a513e2539459
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-01-17 19:18:27 -0300

    passo a passo da automacao

---
+++ 08 - Automatizacao Email/auto.py
@@ -0,0 +1,6 @@
+# 1 - entrar no sistema da empresa (no caso o link do drive
+# 2 - navegar no sistema (até a pasta exportar)
+# 3 - fazer o download da base de vendas
+# 4 - Inportar a base de vendas pro Python
+# 5 - Calcular o faturamente e a quantidade de produtos vendidos (os indicadores)
+# 6 - Enviar o email para a diretoria
