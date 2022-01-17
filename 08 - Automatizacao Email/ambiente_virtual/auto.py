c87a38947d6eb7a76b1c381496369be4d7f00523
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-01-17 19:49:23 -0300

    alguns comandos no pythongui

--- 08 - Automatizacao Email/ambiente_virtual/auto.py
+++ 08 - Automatizacao Email/ambiente_virtual/auto.py
@@ -1,6 +1,14 @@
-# 1 - entrar no sistema da empresa (no caso o link do drive
+# 1 - entrar no sistema da empresa (no caso o link do drive)
 # 2 - navegar no sistema (até a pasta exportar)
 # 3 - fazer o download da base de vendas
 # 4 - Inportar a base de vendas pro Python
 # 5 - Calcular o faturamente e a quantidade de produtos vendidos (os indicadores)
 # 6 - Enviar o email para a diretoria
+
+
+
+# 1 - entrar no sistema da empresa (no caso o link do drive)
+# alguns comandos
+# aperta uma tecla do teclado - pyautogui.press("c")
+# clica com o mouse - pyautogui.click()
+# combinação de teclas - pyautogui.hotkey("ctrl","c")
