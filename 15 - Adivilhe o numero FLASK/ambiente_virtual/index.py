b3bd9415d1fd1689e54ded05def23596d5e51c32
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-02-04 15:07:02 -0300

    templates

--- 15 - Adivilhe o numero FLASK/ambiente_virtual/index.py
+++ 15 - Adivilhe o numero FLASK/ambiente_virtual/index.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def ola_mundo():
-    return 'Meu primeiro site'
+    return render_template("index.html")
 
 @app.route('/sobre')
 def sobre():
