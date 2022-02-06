8e7f53ab37233a9501f8430315bb56a3c2cafb97
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-02-06 12:58:43 -0300

    json api flask

--- 19 - flask data science/ambiente_virtual/app.py
+++ 19 - flask data science/ambiente_virtual/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import jsonify
 
 app = Flask(__name__)
 
@@ -14,5 +15,22 @@ def foo():
 def hi(name):
     return f'Hi {name}!'
 
+@app.route('/api')
+def api():
+    data = {
+        'name': 'Andrew',
+        'user': 'acroz'
+    }
+    return jsonify(data)
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+# O Python tem suporte a JSON em sua biblioteca padrão, 
+# mas ao usar o Flask recomendamos o uso do  jsonify()auxiliar, 
+# que não apenas serializa seus dados para JSON, mas também
+#  prepara um Response objeto Flask com coisas úteis como a
+#   predefinição do tipo de conteúdo HTTP.
+
