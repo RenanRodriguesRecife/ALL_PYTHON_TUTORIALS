610e136d49d3a42b6cdc047acc30def16fb8e473
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-02-10 13:25:15 -0300

    add sklearn

--- 19 - flask data science/ambiente_virtual/app.py
+++ 19 - flask data science/ambiente_virtual/app.py
@@ -1,5 +1,20 @@
 from flask import Flask
 from flask import jsonify
+import numpy
+from sklearn.datasets import make_classification
+from sklearn.linear_model import LogisticRegression
+
+X, y = make_classification(
+    n_samples=100,
+    n_features=2,
+    n_classes=2,
+    n_informative=2,
+    n_redundant=0
+)
+
+model = LogisticRegression()
+model.fit(X,y)
+
 
 app = Flask(__name__)
 
@@ -23,8 +38,9 @@ def api():
     }
     return jsonify(data)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+
+
 
 
 
@@ -46,3 +62,30 @@ if __name__ == '__main__':
 # array_2d = numpy.array([[1., 1.5], [1.5, 2.]])
 # json.dumps([[float(v) for v in row] for row in array_2d])
 # '[[1.0, 1.5], [1.5, 2.0]]'
+
+
+@app.route('/predict/feature_1/<feature_1>/feature_2/<feature_2>')
+def predict(feature_1,feature_2):
+    feature_1 = float(feature_1)
+    feature_2 = float(feature_2)
+
+    #perform model prediction
+    features = numpy.array([[feature_1, feature_2]])
+    predicted_class = model.predict(features)[0]
+    probabilities = model.predict_proba(features)[0]
+
+    #prepare response
+    content = {
+        'class': int(predicted_class),
+        'probabilities': [
+            float(p) for p in probabilities
+        ]
+    }
+
+    return jsonify(content)
+
+#localhost:5000/predict/feature_1/2.0/feature_2/2.0
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
